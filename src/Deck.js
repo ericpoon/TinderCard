cb589e20950907c231e62bd0a35eb8b634fa4c57
PAN Zhiqi <ericpoon510@gmail.com>
2018-03-02 14:17:37 +0800

    Setup PanResponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text, Animated } from 'react-native';
+import {
+  View,
+  Text,
+  Animated,
+  PanResponder,
+} from 'react-native';
 
 class Deck extends Component {
+  constructor() {
+    super();
+
+    // there could be more than 1 pan responders
+    this.panResponder = PanResponder.create({
+      /** lifecycle method: called whenever a user taps on the screen, determines whether or not the pan responder should handle the gesture */
+      onStartShouldSetPanResponder: () => true,
+      /** lifecycle method: called when user is dragging the finger around */
+      onPanResponderMove: (event, gesture) => {
+        // console.log({...gesture}); // reserve a copy
+        /* console.log(gesture)
+         * React and React Native reuses the same object for performance purpose
+         * so here we are logging out the same single gesture object in memory
+         * the second time we access it, it doesn't remain the same value
+         */
+      },
+      /** lifecycle method: called when user removes the finger on screen */
+      onPanResponderRelease: () => {
+      },
+    });
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
@@ -10,8 +37,9 @@ class Deck extends Component {
 
   render() {
     this.renderCards();
+    console.log(this.panResponder.panHandlers);
     return (
-      <View>
+      <View {...this.panResponder.panHandlers}>
         {this.renderCards()}
       </View>
     );
